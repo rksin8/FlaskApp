a40d42e6f4afa231a57494d00c1d2ddae953553b
rksin8 <ranjeetbhu88@gmail.com>
2019-04-15 14:37:15 +1000

    index.html added

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 # basic route
 @app.route("/")
 def main():
-   return "Hello from myblog.com"
+    return render_template('index.html')
+   #return "Hello from myblog.com"
 
 if __name__ == "__main__":
    app.run()
